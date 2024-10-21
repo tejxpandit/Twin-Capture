a96f1c81e88830f92f020d6d5312bb55c7d3213d
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-10-20 19:53:27 -0500

    imread video stream
    Attempt to use imread for the stream acquisition. Still fails.

--- test/test_stream_capture_imageIO.py
+++ test/test_stream_capture_imageIO.py
@@ -12,4 +12,12 @@ URL = "http://192.168.4.27:8080/video"
 # for idx, frame in enumerate(iio.imiter(URL)):
 #     print(f"Frame {idx}: avg. color {np.sum(frame, axis=-1)}")
 
+with iio.imread(
+    URL,  # Replace with your actual MJPEG URL
+    # plugin="FFMPEG",  # Use the FFMPEG plugin
+    # Other FFMPEG parameters can be added here if needed
+) as reader:
+    for i, im in enumerate(reader):
+        print(f"Frame {i}: {im.shape}")
+  
 # FAILS EVERY TIME
