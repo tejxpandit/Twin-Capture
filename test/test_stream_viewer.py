32e1d87f2f5e911d4bc491d75675e96fcaf0bdba
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-10-23 22:43:08 -0500

    Updated Test : Stream Viewer
    Updated the stream viewer test to use a video stream from a URL.

--- test/test_stream_viewer.py
+++ test/test_stream_viewer.py
@@ -10,59 +10,40 @@ from time import sleep
 
 URL = "http://192.168.4.27:8080/video"
 
+key = cv2.waitKey(1)
+# webcam = cv2.VideoCapture(URL)
+webcam = cv2.VideoCapture(0)
 
-def CaptureAndView():
-    key = cv2. waitKey(1)
-    # webcam = cv2.VideoCapture(URL)
-    webcam = cv2.VideoCapture(0)
-    sleep(2)
+frame_number = 0
+while True:
 
-    frame_number = 0
-    while True:
+    try:
+        check, frame = webcam.read()
+        cv2.imshow("Capturing", frame)
 
-        try:
-            check, frame = webcam.read()
-            cv2.imshow("Capturing", frame)
-    
-            if(frame_number > 1):
-                img = cv2.resize(frame,(640,480))
-                img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-                
-
-                frame_number = 0
-            else:
-                frame_number += 1
+        if(frame_number > 1):
+            img = cv2.resize(frame,(640,480))
+            img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
             
-            #results.show()
 
-            key = cv2.waitKey(1)
-            if key == ord('q'):
-                webcam.release()
-                cv2.destroyAllWindows()
-                break
+            frame_number = 0
+        else:
+            frame_number += 1
+
         
-        except(KeyboardInterrupt):
-            print("Turning off camera.")
+        #results.show()
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
             webcam.release()
-            print("Camera off.")
-            print("Program ended.")
             cv2.destroyAllWindows()
             break
-
-# DPG Context
-dpg.create_context()
-
-# DPG Viewport
-dpg.create_viewport(title="Project Title", width=600, height=200)
-
-# DPG Window
-dpg.add_window(label="viewer", tag="viewer_window")
-CaptureAndView()
-
-# DPG Render Context
-dpg.setup_dearpygui()
-dpg.show_viewport()
-dpg.start_dearpygui()
-dpg.destroy_context()
+    
+    except(KeyboardInterrupt):
+        print("Turning off camera.")
+        webcam.release()
+        print("Camera off.")
+        print("Program ended.")
+        cv2.destroyAllWindows()
+        break
