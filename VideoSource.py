5a177c1257e507e7b39ebd3dd0d8d4e7fa027444
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-10-26 23:12:03 -0500

    Video Source Parameters
    Created the base internal parameters of the Video Source Object.

--- VideoSource.py
+++ VideoSource.py
@@ -10,3 +10,15 @@ from util.ImageView import ImageView
 class VideoSource:
     def __init__(self, video_src_id=0, video_sources=[]):
         self.id = video_src_id
+        self.video_sources = video_sources
+        self.name = "Video " + str(video_src_id)
+        self.type = None
+        self.source_types = ["Camera", "IP/Mobile"]
+        self.sources = []
+        self.video_width = 400
+        self.video_height = 100
+        self.streaming_state = False
+        self.recording_state = False
+        self.ip_address = None
+        self.port_address = None
+        self.cam_src = None
