efd35303a94be547a1adc5fb72da7f1721a7c486
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-11-02 23:39:48 -0500

    Test : Dynamic Image Update
    Created a test for dynamically updating images.

---
+++ util/test_change_img_tex.py
@@ -0,0 +1,11 @@
+# File : Test : Chaning Image Texture
+# Author : Tej Pandit
+# Date : Oct 2024
+
+import dearpygui.dearpygui as dpg
+from ImageView import ImageView
+import time
+
+def update():
+    ImgViewer.updateImage("TestImage3.png", "jelly")
+
