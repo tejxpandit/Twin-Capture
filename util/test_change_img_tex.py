5c201cdc13904d2f283eb967eff083685b31b6c9
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-11-02 23:41:29 -0500

    Test : Dynamic Image Update
    Used the Image Viewer class from DPG-Utilities to create an image viewer object which can add/update images.

--- util/test_change_img_tex.py
+++ util/test_change_img_tex.py
@@ -9,3 +9,22 @@ import time
 def update():
     ImgViewer.updateImage("TestImage3.png", "jelly")
 
+# DPG Context
+dpg.create_context()
+# DPG Viewport
+dpg.create_viewport(title="Image Viewer", width=600, height=300)
+# DPG Window
+dpg.add_window(label="Image Viewer", tag="img_view")
+
+dpg.add_button(label="Update", parent="img_view", tag="update_button", callback=update)
+dpg.add_group(parent="img_view", tag="mygroup", horizontal=True, width=400, height=400)
+
+# DPG Image Viewer Example
+ImgViewer = ImageView()
+ImgViewer.newImage("TestImage.png", parent="img_view", tag="jelly")
+
+# DPG Render Context
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
